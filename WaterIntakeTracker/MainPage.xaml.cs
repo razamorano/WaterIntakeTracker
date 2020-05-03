1c6e7827fe9f79affb0b37c13066b128031acf8d
Ramon Zamorano <razamorano@gmail.com>
2020-05-03 19:05:43 +1200

    Making updates on formulas
    adding formulas to the calculation

--- WaterIntakeTracker/MainPage.xaml.cs
+++ WaterIntakeTracker/MainPage.xaml.cs
@@ -18,9 +18,9 @@ namespace WaterIntakeTracker
         Entry workoutLBL;
         Entry bottlesizeLBL;
         Button calculateBTN;
-        Button callButton;
-
-        string wateramount;
+        Button clearBTN;
+        Entry waterltLBL;
+        Entry waterbottleLBL;
 
         public MainPage()
         {
@@ -51,19 +51,19 @@ namespace WaterIntakeTracker
 
             panel.Children.Add(workoutLBL = new Entry
             {
-                Text = "Workout",
+                Text = "0",
             });
 
             //Bottle size
             panel.Children.Add(new Label
             {
-                Text = "What is you bottle size on mL?",
+                Text = "What is your bottle size on ml?",
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
             });
 
             panel.Children.Add(bottlesizeLBL = new Entry
             {
-                Text = "mL",
+                Text = "0",
             });
 
             //Calculate button                       
@@ -73,16 +73,50 @@ namespace WaterIntakeTracker
                 //IsEnabled = false,
             });
 
+            panel.Children.Add(waterltLBL = new Entry
+            {
+                Text = "",
+            });
+
+            panel.Children.Add(waterbottleLBL = new Entry
+            {
+                Text = "",
+            });
+
+            //Clear button                     
+            panel.Children.Add(clearBTN = new Button
+            {
+                Text = "Clear entries",
+            });
+
             calculateBTN.Clicked += OnCalculate;
+            clearBTN.Clicked += OnClear;
+
             this.Content = panel;
         }
 
-        private void OnCalculate(object sender, EventArgs e)
+        private void OnCalculate(object sender, System.EventArgs e)
         {
-            int enterWeight = Int32.Parse(weightLBL.Text);
+            //convert string variables on integers
+            float enterWeight = float.Parse(weightLBL.Text);
             int enterWorkout = Int32.Parse(workoutLBL.Text);
             int enterBottlesize = Int32.Parse(bottlesizeLBL.Text);
-
+            //to calculate the water intake I have use as a reference a formula that it take the mass on pounds and multipli by 2/3
+            //then it will give you the intake on ounces, after this we have to add the workout consume of water and lastly device to 33.814 to pass from
+            //ounce to liters
+            float waterinLt = (((enterWeight * 2.20462f) * 0.6666666f) + (enterWorkout * 0.4f)) / 33.814f;
+            // just print out with 2 decimals
+            waterltLBL.Text = waterinLt.ToString("0.00") + " Liters of Water you should intake a day";
+            // now calculate how many bottle
+            waterbottleLBL.Text = ((waterinLt * 1000) / enterBottlesize).ToString("0.0") + " Bottles of Water you should take a day";
+        }
+        private void OnClear(object sender, System.EventArgs e)
+        {
+            weightLBL.Text = "Kg.";
+            workoutLBL.Text = "0";
+            bottlesizeLBL.Text = "0";
+            waterltLBL.Text = "";
+            waterbottleLBL.Text = "";
         }
     }
 }
